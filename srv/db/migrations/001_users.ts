6f6195d4e5b04990f2382d960ea8034eb8c2b839
Tom Ierino <tierino@mypassglobal.com>
2021-06-12 14:19:21 +0800

    oops

--- srv/db/migrations/001_users.ts
+++ srv/db/migrations/001_users.ts
@@ -1,7 +1,5 @@
 import { Knex } from "knex"
 import { tables } from "../"
-import { table } from "../schema"
-import { v4 as uuidv4 } from "uuid"
 
 export async function up(db: Knex) {
   const exists = await db.schema.hasTable(tables.users)
@@ -11,12 +9,6 @@ export async function up(db: Knex) {
       tbl.dateTime("created_at")
       tbl.text("username")
       tbl.text("hash")
-    })
-    await table.users().insert({
-      user_id: uuidv4(),
-      username: "init_user",
-      created_at: new Date(),
-      hash: "123",
     })
   }
 }
